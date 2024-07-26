1f6d2b515125ff71b1c16161c6e904d80cfe38c3
ramontrndd <ramonbraintrindade@gmail.com>
2024-07-26 01:18:24 -0300

    chore: Update project configuration files and add SCSS files for components and utilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,31 @@ import { CarrouselComponent } from 'src/shared/carrousel/carrousel.component';
 })
 export class AppComponent {
   title = 'tucujuprevi';
+
+  Images = [
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1460627390041-532a28402358?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'nature1',
+      href: 'https://www.google.com',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'nature2',
+      href: 'https://www.google.com',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1640844444545-66e19eb6f549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+      imageAlt: 'person1',
+      href: 'https://www.google.com',
+    },
+    {
+      imageSrc:
+        'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'person2',
+      href: 'https://www.google.com',
+    },
+  ];
 }
