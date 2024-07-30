83b4d7d17293857635b146413420d84345f2e3bc
ramontrndd <ramonbraintrindade@gmail.com>
2024-07-30 17:04:34 -0300

    chore: Remove unused import in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { CommonModule } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
