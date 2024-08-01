4c43153094a6423bc86d4aad843882acb29d1ff5
ramontrndd <ramonbraintrindade@gmail.com>
2024-07-31 22:25:10 -0300

    feat: add button scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardExplainComponent } from 'src/shared/card-explain/card-explain.component';
 import { CardIconsComponent } from 'src/shared/card-icons/card-icons.component';
@@ -8,6 +8,7 @@ import { CarrouselComponent } from 'src/shared/carrousel/carrousel.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -21,10 +22,34 @@ import { HeaderComponent } from './components/header/header.component';
     CardIconsComponent,
     CardExplainComponent,
     ContactComponent,
+    MatIconModule,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'tucujuprevi';
+  isVisible = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop =
+      window.scrollY ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    const windowHeight =
+      window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight ||
+      0;
+    const documentHeight =
+      document.documentElement.scrollHeight || document.body.scrollHeight || 0;
+
+    this.isVisible = scrollTop + windowHeight >= documentHeight;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
